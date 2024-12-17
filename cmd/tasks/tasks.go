4a2eb0ab164deea23b784159cc0361a4aee91783
timwehrle <wetigg@gmail.com>
2024-12-17 21:05:54 +0100

    fix(tasks): update code for better readability

--- cmd/tasks/tasks.go
+++ cmd/tasks/tasks.go
@@ -2,6 +2,7 @@ package tasks
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -14,99 +15,125 @@ import (
 var Cmd = &cobra.Command{
 	Use:   "tasks",
 	Short: "Manage tasks",
-	Run: func(cmd *cobra.Command, args []string) {
-		token, err := auth.Get()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		client := api.New(token)
-
-		tasks, err := client.GetTasks()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		if len(tasks) == 0 {
-			fmt.Println("No tasks found.")
-			return
-		}
-
-		taskNames := make([]string, len(tasks))
-		for i, task := range tasks {
-			taskNames[i] = fmt.Sprintf("[%s] %s", utils.FormatDate(task.DueOn), task.Name)
-		}
-
-		today := time.Now()
-
-		selectMessage := fmt.Sprintf("Your Tasks on %s (Select one for more details):", today.Format("Jan 02, 2006"))
-
-		index, err := prompter.Select(selectMessage, taskNames)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		selectedTask := tasks[index]
-
-		task, err := client.GetTask(selectedTask.GID)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		fmt.Printf("%s [%s], %s\n", utils.BoldUnderline.Sprint(task.Name), utils.FormatDate(task.DueOn), displayProjects(task.Projects))
-		fmt.Println(displayTags(task.Tags))
-		fmt.Print(displayNotes(task.Notes))
-
-		confirm := false
-		confirm, err = prompter.Confirm("Do you want to mark the task as done?", "No")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		if confirm {
-			err := client.MarkTaskAsDone(selectedTask.GID)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-
-			fmt.Println("Task successfully marked as done.")
-		}
-	},
+	Run:   run,
 }
 
-func displayProjects(projects []api.Project) string {
+func run(cmd *cobra.Command, args []string) {
+	token, err := auth.Get()
+	if err != nil {
+		log.Printf("Failed to authenticate: %v", err)
+	}
+
+	client := api.New(token)
+
+	tasks, err := fetchTasks(client)
+	if err != nil {
+		log.Printf("Failed to fetch tasks: %v", err)
+	}
+
+	selectedTask, err := promptForTaskSelection(tasks)
+	if err != nil {
+		log.Printf("Failed to select a task: %v", err)
+	}
+
+	if err := displayTaskDetails(client, selectedTask); err != nil {
+		log.Printf("Failed to display task details: %v", err)
+	}
+
+	if err := handleTaskCompletion(client, selectedTask); err != nil {
+		log.Printf("Failed to mark task as done: %v", err)
+	}
+}
+
+func fetchTasks(client *api.Client) ([]api.Task, error) {
+	tasks, err := client.GetTasks()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(tasks) == 0 {
+		fmt.Println("No tasks found.")
+		return nil, nil
+	}
+
+	return tasks, nil
+}
+
+func promptForTaskSelection(tasks []api.Task) (*api.Task, error) {
+	taskNames := formatTaskNames(tasks)
+
+	today := time.Now()
+	selectMessage := fmt.Sprintf("Your Tasks on %s (Select one for more details):", today.Format("Jan 02, 2006"))
+
+	index, err := prompter.Select(selectMessage, taskNames)
+	if err != nil {
+		return nil, err
+	}
+
+	return &tasks[index], nil
+}
+
+func formatTaskNames(tasks []api.Task) []string {
+	taskNames := make([]string, len(tasks))
+	for i, task := range tasks {
+		taskNames[i] = fmt.Sprintf("[%s] %s", utils.FormatDate(task.DueOn), task.Name)
+	}
+	return taskNames
+}
+
+func displayTaskDetails(client *api.Client, task *api.Task) error {
+	detailedTask, err := client.GetTask(task.GID)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%s [%s], %s\n", utils.BoldUnderline.Sprint(detailedTask.Name), utils.FormatDate(detailedTask.DueOn), formatProjects(detailedTask.Projects))
+	fmt.Println(formatTags(detailedTask.Tags))
+	fmt.Print(formatNotes(detailedTask.Notes))
+
+	return nil
+}
+
+func handleTaskCompletion(client *api.Client, task *api.Task) error {
+	confirm, err := prompter.Confirm("Do you want to mark the task as done?", "No")
+	if err != nil {
+		return err
+	}
+
+	if confirm {
+		if err := client.MarkTaskAsDone(task.GID); err != nil {
+			return err
+		}
+		fmt.Println("Task successfully marked as done.")
+	}
+	return nil
+}
+
+func formatProjects(projects []api.Project) string {
 	if len(projects) > 0 {
-		var projectNames []string
-		for _, project := range projects {
-			projectNames = append(projectNames, project.Name)
+		projectNames := make([]string, len(projects))
+		for i, project := range projects {
+			projectNames[i] = project.Name
 		}
 		return "Projects: " + fmt.Sprintf("%s", projectNames)
 	}
-
 	return "Projects: None"
 }
 
-func displayTags(tags []api.Tag) string {
+func formatTags(tags []api.Tag) string {
 	if len(tags) > 0 {
-		var tagNames []string
-		for _, tag := range tags {
-			tagNames = append(tagNames, tag.Name)
+		tagNames := make([]string, len(tags))
+		for i, tag := range tags {
+			tagNames[i] = tag.Name
 		}
 		return "Tags: " + fmt.Sprintf("%s", tagNames)
 	}
 	return "Tags: None"
 }
 
-func displayNotes(notes string) string {
+func formatNotes(notes string) string {
 	if notes != "" {
 		return fmt.Sprintf("\n%s\n", notes)
 	}
-
 	return ""
 }
