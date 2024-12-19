e525d07f64ad967bbd783545485619b5924af968
timwehrle <wetigg@gmail.com>
2024-12-19 11:05:07 +0100

    fix(logout): update command error handling

--- cmd/auth/logout/logout.go
+++ cmd/auth/logout/logout.go
@@ -28,34 +28,32 @@ var Cmd = &cobra.Command{
 		# Start logout process
 		$ act auth logout
 	`),
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		_, err := auth.Get()
 		if errors.Is(err, auth.ErrNoToken) {
 			fmt.Println("No active session found. You are not logged in.")
-			return
+			return nil
 		}
 		if err != nil {
-			fmt.Println("Error retrieving user details:", err)
-			return
+			return err
 		}
 
 		confirm := false
 		confirm, err = prompter.Confirm("Are you sure you want to log out?", "No")
 		if err != nil {
-			fmt.Println("Error reading confirmation:", err)
-			return
+			return err
 		}
 
 		if confirm {
 			err := auth.Delete()
 			if err != nil {
-				fmt.Println("Error deleting credentials:", err)
-				return
+				return err
 			}
-
 			fmt.Println("Successfully logged out.")
 		} else {
 			fmt.Println("Logout aborted.")
 		}
+
+		return nil
 	},
 }
