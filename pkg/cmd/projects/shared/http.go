9a533e884448e335e0c1d84038c8f4c4a04cb54a
Tim <115550741+timwehrle@users.noreply.github.com>
2025-01-11 20:40:28 +0100

    Add projects tasks functionality (#12)
    * feat: add projects tasks command
    
    * fix: update functions

---
+++ pkg/cmd/projects/shared/http.go
@@ -0,0 +1,38 @@
+package shared
+
+import "github.com/timwehrle/asana-go"
+
+func FetchAllProjects(client *asana.Client, workspace *asana.Workspace, limit int) ([]*asana.Project, error) {
+	initialCapacity := 100
+	if limit > 0 {
+		initialCapacity = limit
+	}
+
+	projects := make([]*asana.Project, 0, initialCapacity)
+	options := &asana.Options{
+		Limit:  limit,
+		Fields: []string{"name"},
+	}
+
+	for {
+		batch, nextPage, err := workspace.Projects(client, options)
+		if err != nil {
+			return nil, err
+		}
+
+		projects = append(projects, batch...)
+
+		if limit > 0 && len(projects) >= limit {
+			projects = projects[:limit]
+			break
+		}
+
+		if nextPage == nil || nextPage.Offset == "" {
+			break
+		}
+
+		options.Offset = nextPage.Offset
+	}
+
+	return projects, nil
+}
---
+++ pkg/cmd/projects/tasks/tasks.go
@@ -0,0 +1,92 @@
+package tasks
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/timwehrle/asana-go"
+	"github.com/timwehrle/asana/pkg/cmd/projects/shared"
+	"github.com/timwehrle/asana/pkg/factory"
+	"github.com/timwehrle/asana/pkg/iostreams"
+)
+
+type TasksOptions struct {
+	factory.Factory
+	IO *iostreams.IOStreams
+}
+
+func NewCmdTasks(f factory.Factory) *cobra.Command {
+	opts := &TasksOptions{
+		Factory: f,
+		IO:      f.IOStreams(),
+	}
+
+	cmd := &cobra.Command{
+		Use:   "tasks",
+		Short: "List tasks of a project",
+		Long:  "Retrieve and display a list of all tasks under a project.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runTasks(opts)
+		},
+	}
+
+	return cmd
+}
+
+func runTasks(opts *TasksOptions) error {
+	cfg, err := opts.Factory.Config()
+	if err != nil {
+		return err
+	}
+
+	client, err := opts.NewAsanaClient()
+	if err != nil {
+		return err
+	}
+
+	var projects []*asana.Project
+	workspace := &asana.Workspace{
+		ID: cfg.Workspace.ID,
+	}
+
+	projects, err = shared.FetchAllProjects(client, workspace, 0)
+	if err != nil {
+		return err
+	}
+
+	projectNames := make([]string, 0, len(projects))
+	for _, project := range projects {
+		projectNames = append(projectNames, project.Name)
+	}
+
+	index, err := opts.Prompter().Select("Select a project:", projectNames)
+	if err != nil {
+		return fmt.Errorf("failed selecting a project: %w", err)
+	}
+
+	project := projects[index]
+
+	tasks, _, err := project.Tasks(client)
+	if err != nil {
+		return fmt.Errorf("failed fetching tasks of project %s: %w", project.Name, err)
+	}
+
+	return printTasks(opts, project, tasks)
+}
+
+func printTasks(opts *TasksOptions, project *asana.Project, tasks []*asana.Task) error {
+	cs := opts.IO.ColorScheme()
+
+	fmt.Fprintf(opts.IO.Out, "\nTasks in %s:\n\n", cs.Bold(project.Name))
+
+	if len(tasks) == 0 {
+		fmt.Fprintf(opts.IO.Out, "No tasks found\n")
+		return nil
+	}
+
+	for i, task := range tasks {
+		fmt.Fprintf(opts.IO.Out, "%d. %s\n", i+1, cs.Bold(task.Name))
+	}
+
+	return nil
+}
