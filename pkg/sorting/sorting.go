11178d6545b663e06d0af175a94ad733f31635fa
Tim Wehrle <hello@timwehrle.de>
2025-05-14 20:37:02 +0200

    refactor: remove unused sorting functions

--- pkg/sorting/sorting.go
+++ pkg/sorting/sorting.go
@@ -2,24 +2,7 @@ package sorting
 
 import (
 	"sort"
-
-	"cmp"
 )
-
-func By[T any](slice []T, less func(a T, b T) bool) {
-	sort.Slice(slice, func(i, j int) bool {
-		return less(slice[i], slice[j])
-	})
-}
-
-func ByField[T any, F cmp.Ordered](slice []T, field func(T) F, descending bool) {
-	sort.Slice(slice, func(i, j int) bool {
-		if descending {
-			return field(slice[i]) > field(slice[j])
-		}
-		return field(slice[i]) < field(slice[j])
-	})
-}
 
 func Sort[T any](slice []T, by func(a, b T) bool) {
 	sort.Slice(slice, func(i, j int) bool {
