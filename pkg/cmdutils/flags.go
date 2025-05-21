74b1e3962711282f3fa95e2e9015bc79ebc96239
Tim Wehrle <hello@timwehrle.de>
2025-05-21 13:17:01 +0200

    refactor: simplify ValidateStringEnum function using slices package

--- pkg/cmdutils/flags.go
+++ pkg/cmdutils/flags.go
@@ -2,6 +2,7 @@ package cmdutils
 
 import (
 	"fmt"
+	"slices"
 	"sort"
 	"strings"
 	"time"
@@ -9,17 +10,15 @@ import (
 
 // ValidateStringEnum returns an error if val is not one of allowed.
 func ValidateStringEnum(flagName, val string, allowed []string) error {
-	for _, a := range allowed {
-		if val == a {
-			return nil
-		}
+	if slices.Contains(allowed, val) {
+		return nil
 	}
-
-	copyAllowed := append([]string(nil), allowed...)
+	copyAllowed := slices.Clone(allowed)
 	sort.Strings(copyAllowed)
 	return fmt.Errorf("invalid value %q for flag --%s; valid values are: %s", val, flagName, strings.Join(copyAllowed, ", "))
 }
 
+// ValidateDate returns an error if val is not a valid date in YYYY-MM-DD format.
 func ValidateDate(flagName, val string) error {
 	if val == "" {
 		return nil
